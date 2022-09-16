fd29354ade67876b474c648e2f45b8d36331d791
Armen <madarmen1987@gmail.com>
2022-09-17 01:46:14 +0300

    Work with ViewController

--- TraficLight/ViewController.swift
+++ TraficLight/ViewController.swift
@@ -8,12 +8,42 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    @IBOutlet var redColorView: UIView!
+    @IBOutlet var yellowCollrView: UIView!
+    @IBOutlet var greenColorView: UIView!
+    @IBOutlet var startButton: UIButton!
+    
+    var tapped = true
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        redColorView.layer.cornerRadius = 85
+        yellowCollrView.layer.cornerRadius = 85
+        greenColorView.layer.cornerRadius = 85
+        startButton.layer.cornerRadius = 15
     }
-
-
+    
+    @IBAction func startButtonTapped() {
+        startButton.setTitle("NEXT", for: .normal)
+       
+        if tapped {
+            tapped = false
+            redColorView.alpha = 1
+        } else if redColorView.alpha == 1{
+            redColorView.alpha = 0.3
+            yellowCollrView.alpha = 1
+        } else if yellowCollrView.alpha == 1 {
+            yellowCollrView.alpha = 0.3
+            greenColorView.alpha = 1
+        } else if greenColorView.alpha == 1 {
+            greenColorView.alpha = 0.3
+            redColorView.alpha = 1
+        }
+        
+    }
 }
+
+
+
+
 
